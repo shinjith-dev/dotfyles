bc57a0976401fe72a508e21e67438f0d78cdc306
shinjith-dev <shinjithpallikkara@gmail.com>
2025-05-12 21:04:07 +0530

    feat: back.up
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

---
+++ nvim/lua/plugins/tailwind-tools.lua
@@ -0,0 +1,22 @@
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim", -- optional
+    "neovim/nvim-lspconfig", -- optional
+  },
+  opts = {
+settings = {
+    tailwindCSS = {
+      experimental = {
+        classRegex = {
+          { "cva\\(((?:[^()]|\\([^()]*\\))*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+          { "cx\\(((?:[^()]|\\([^()]*\\))*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" }
+        },
+      },
+    },
+  },
+  }
+}
